8328fe60128e57bb7499ed6668db20adf97d744d
ZXMJ <1343421239@qq.com>
2017-05-31 15:45:27 +0800

    mybatis

---
+++ src/main/java/com/mapper/OrderdetailMapper.java
@@ -0,0 +1,8 @@
+package com.mapper;
+
+public interface OrderdetailMapper {
+
+	//根据id查询商品信息
+	public  void findOrderdetailById() throws Exception;
+	
+}
---
+++ src/main/java/com/po/UserCustom.java
@@ -0,0 +1,20 @@
+package com.po;
+
+import com.po.User;
+
+/**
+ * 
+ * <p>Title: UserCustom</p>
+ * <p>Description: 用户的扩展类</p>
+ * <p>Company: www.itcast.com</p> 
+ * @author	传智.燕青
+ * @date	2015-4-22下午4:26:31
+ * @version 1.0
+ */
+public class UserCustom extends User{
+
+	private static final long serialVersionUID = -281523988378120674L;
+	
+	//可以扩展用户的信息
+
+}
